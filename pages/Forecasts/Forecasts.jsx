b881e998df0c6cf70da255bba71174e4baba2bc5
mutzon <martinutzon@gmail.com>
2025-04-16 20:15:56 +0200

    færdig med 81

--- pages/Forecasts/Forecasts.jsx
+++ pages/Forecasts/Forecasts.jsx
@@ -1,9 +1,14 @@
 import { s } from "./Forecasts.style.js";
 import { Txt } from "../../components/Txt/Txt.jsx";
 import { useRoute } from "@react-navigation/native";
+import { Header } from "../../components/Header/Header.jsx";
 
 export function Forecasts({}) {
   const { params } = useRoute();
-  console.log(params);
-  return <Txt>Forecasts</Txt>;
+  //  console.log(params);
+  return (
+    <>
+      <Header city={params.city}></Header>
+    </>
+  );
 }
